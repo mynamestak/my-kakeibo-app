8b5a64e3f08e43ea9c8c6ed4cf46bfcbfa2f08b7
スワタクヤ <suwatakuya@suwatakuyanoMacBook-Air.local>
2022-03-16 17:11:08 +0900

    enable to edit text

--- src/components/IncomeList.js
+++ src/components/IncomeList.js
@@ -1,19 +1,51 @@
 import React, {useState} from 'react';
 
-const IncomeList = ({income, incomes, incomeText, setIncomes, incomeNum, incomeOrder, incomeAmounts, setIncomeAmounts, setDecreNum, decreNum, id}) => {
+const IncomeList = ({income, incomes, incomeText, setIncomes, incomeNum, incomeAmounts, setIncomeAmounts, id, index}) => {
+
+    const [ isEditting, setIsEditting ] = useState(null);
+    const [ editValue, setEditValue ] = useState('');
+
     const incomeDeleteHandler = () => {
         setIncomes(incomes.filter(el => el.id !== income.id));
-        // setDecreNum(incomeAmounts.slice(incomeOrder, incomeOrder + 1));
         setIncomeAmounts(incomeAmounts.filter(el => el !== Number(incomeNum)));
     }
+
+    const editToggle = () => {
+        setIsEditting(id + 1); //←nullの状態から数字を入れるとtrueになる。
+        setEditValue(incomeText);
+    };
+
+    const editValueHandler = (e) => {
+        setEditValue(e.target.value);
+    };
+
+    const onHandleEdit = () => {
+        const updated = incomes.map((income, index) => {
+            if(income.id === isEditting - 1){ //配列の数字とIncomeFormで作成するidを比較する。
+                income = {incomeText: editValue, incomeNum: incomeNum, id: id}; // incomeTextだけ変更を反映する。
+            }
+            return income;
+        })
+        setIncomes(updated); // 変更した配列丸ごとで更新
+        setIsEditting(false);
+    };
+
 
 
     return(
         <div key={id}>
-            <li>{incomeText}が{incomeNum}でした。</li>
-            {/* <li>{incomeOrder}</li> */}
-            {/* <li>記入日：</li> */}
-            <button onClick={incomeDeleteHandler}>削除</button>
+            { !isEditting &&
+                <li>
+                    <span onClick={editToggle}>{incomeText}</span>に<span>{incomeNum}</span>稼ぎました。
+                    <button onClick={incomeDeleteHandler}>削除</button>
+                </li>            
+            }
+            { isEditting && 
+                <li>
+                    <input onChange={editValueHandler} value={editValue}></input>
+                    <button onClick={onHandleEdit}>決定</button>
+                </li>     
+            }
         </div>
     );
 }
